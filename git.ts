bc17516588306179c3c29b2bfb3e95bd75da6137
yukimemi <yukimemi@gmail.com>
2023-06-10 23:59:29 +0900

    :zap: Use local file info for git get revision and branch name.

--- git.ts
+++ git.ts
@@ -1,12 +1,48 @@
 import * as git from "https://esm.sh/simple-git@3.19.0";
+import * as path from "https://deno.land/std@0.191.0/path/mod.ts";
+import { exists } from "https://deno.land/std@0.191.0/fs/exists.ts";
+import { readLines } from "https://deno.land/std@0.191.0/io/mod.ts";
 
 export class Git {
   public g: git.SimpleGit;
+  public gitDir: string;
 
-  constructor(
-    public base: string,
-  ) {
+  constructor(public base: string) {
     this.g = git.simpleGit(base).env({ ...Deno.env.toObject() });
+    this.gitDir = path.join(base, ".git");
+  }
+
+  public async getHead(): Promise<string> {
+    const headFile = path.join(this.gitDir, "HEAD");
+    return (await Deno.readTextFile(headFile)).trim();
+  }
+
+  public async getRevision(): Promise<string> {
+    const head = await this.getHead();
+    const ref = head.substring(5);
+    const refFile = path.join(this.gitDir, ref);
+    if (await exists(refFile)) {
+      const ref = await Deno.readTextFile(refFile);
+      return ref.split("\n")[0].trim();
+    }
+    for await (
+      const line of readLines(
+        await Deno.open(path.join(this.gitDir, "packed-refs")),
+      )
+    ) {
+      if (line.match(`/ ${ref}/`)) {
+        return line.split(" ")[0];
+      }
+    }
+    return await this.g.revparse("HEAD");
+  }
+
+  public async getBranch(): Promise<string> {
+    const head = await this.getHead();
+    if (head.match(/^ref: refs\/heads\//)) {
+      return head.substring(16);
+    }
+    return (await this.g.branch()).current;
   }
 
   public static async clone(url: string, dst: string, branch?: string) {
@@ -17,15 +53,17 @@ export class Git {
   }
 
   public async pull(branch?: string) {
-    const head = await this.g.revparse("HEAD");
-    const currentBranch = (await this.g.branch()).current;
+    const head = await this.getRevision();
+    const currentBranch = await this.getBranch();
     branch ??= currentBranch;
-    const remote = (await this.g.getRemotes())[0].name;
-    console.log(`Update ${this.base}, remote: ${remote}, branch: ${branch}`);
+    console.log(`Update ${this.base}, branch: ${branch}`);
     if (branch !== currentBranch) {
       this.g.checkout(branch);
     }
-    const pullResult = await this.g.pull(remote, branch);
+    const pullResult = await this.g.pull([
+      "--ff-only",
+      "--rebase=false",
+    ]);
     if (pullResult && pullResult.summary.changes) {
       return this.g.log({ from: head });
     }
---
+++ git_test.ts
@@ -0,0 +1,37 @@
+import * as git from "https://esm.sh/simple-git@3.19.0";
+import { assertEquals } from "https://deno.land/std@0.191.0/testing/asserts.ts";
+
+import { Git } from "./git.ts";
+
+async function init() {
+  const repo = "https://github.com/yukimemi/dvpm";
+  const dst = await Deno.makeTempDir();
+  await git.simpleGit().clone(repo, dst);
+  return git.simpleGit(dst).env({ ...Deno.env.toObject() });
+}
+
+Deno.test({
+  name: "Test getRevisiont",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  fn: async () => {
+    const g = await init();
+    const expected = await g.revparse("HEAD");
+    const git = new Git(await g.revparse(["--show-toplevel"]));
+    const actual = await git.getRevision();
+    assertEquals(actual, expected);
+  },
+});
+
+Deno.test({
+  name: "Test getBranch",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  fn: async () => {
+    const g = await init();
+    const expected = (await g.branch()).current;
+    const git = new Git(await g.revparse(["--show-toplevel"]));
+    const actual = await git.getBranch();
+    assertEquals(actual, expected);
+  },
+});
