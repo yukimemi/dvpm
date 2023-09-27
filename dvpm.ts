89382c39ed2e8b11750ecc4df60302fcf5bb16cb
GitHub <noreply@github.com>
2023-09-27 06:05:56 +0000

    chore(deps): Update deno dependencies

--- dvpm.ts
+++ dvpm.ts
@@ -2,10 +2,10 @@ import * as buffer from "https://deno.land/x/denops_std@v5.0.1/buffer/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 import { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 import { Semaphore } from "https://deno.land/x/async@v2.0.2/semaphore.ts";
-import { assert, is } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
+import { assert, is } from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
 import { cache, notify } from "./util.ts";
 import { echo, execute } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
-import { sprintf } from "https://deno.land/std@0.202.0/fmt/printf.ts";
+import { sprintf } from "https://deno.land/std@0.203.0/fmt/printf.ts";
 import { type Plug, Plugin, PluginOption } from "./plugin.ts";
 
 const concurrency = 8;
