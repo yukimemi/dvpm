500ea57f12a7e6625125885bb5093c933c3e143c
GitHub <noreply@github.com>
2024-02-01 06:05:37 +0000

    chore(deps): Update deno dependencies

--- util.ts
+++ util.ts
@@ -6,9 +6,9 @@
 
 import type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
 import { echo, echoerr, execute } from "https://deno.land/x/denops_std@v5.3.0/helper/mod.ts";
-import * as fs from "https://deno.land/std@0.213.0/fs/mod.ts";
+import * as fs from "https://deno.land/std@0.214.0/fs/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v5.3.0/function/mod.ts";
-import { dirname, extname } from "https://deno.land/std@0.213.0/path/mod.ts";
+import { dirname, extname } from "https://deno.land/std@0.214.0/path/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 
 /**
