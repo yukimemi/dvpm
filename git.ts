6c2fd4c13b940605fbf4b8f0aca08d718822262a
GitHub <noreply@github.com>
2024-03-15 06:05:59 +0000

    chore(deps): Update deno dependencies

--- git.ts
+++ git.ts
@@ -4,9 +4,9 @@
 // Last Change : 2024/01/06 01:09:58.
 // =============================================================================
 
-import * as path from "https://deno.land/std@0.219.1/path/mod.ts";
-import { exists } from "https://deno.land/std@0.219.1/fs/exists.ts";
-import { TextLineStream } from "https://deno.land/std@0.219.1/streams/mod.ts";
+import * as path from "https://deno.land/std@0.220.1/path/mod.ts";
+import { exists } from "https://deno.land/std@0.220.1/fs/exists.ts";
+import { TextLineStream } from "https://deno.land/std@0.220.1/streams/mod.ts";
 
 /**
  * Git
