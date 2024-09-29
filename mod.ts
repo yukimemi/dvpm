3170eb7feae9fe114b4b2b2e6cfc17f6e38d5c3a
yukimemi <yukimemi@gmail.com>
2024-09-29 15:25:03 +0900

    :zap: fix export.

--- mod.ts
+++ mod.ts
@@ -1,9 +1,10 @@
 // =============================================================================
 // File        : mod.ts
 // Author      : yukimemi
-// Last Change : 2024/06/30 13:00:11.
+// Last Change : 2024/09/29 15:24:37.
 // =============================================================================
 
 export * from "./dvpm.ts";
 export * from "./plugin.ts";
 export * from "./util.ts";
+export * from "./types.ts";
